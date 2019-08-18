2821c62ded6b510bf334d70ebddeb3da3ce05dfe
sglee <soongoo@gmail.com>
2019-08-18 15:30:15 +0900

    init

---
+++ src/main/java/kr/co/lsg/hello/HelloLogger.java
@@ -0,0 +1,15 @@
+package kr.co.lsg.hello;
+
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloLogger {
+	private static final Logger logger = LoggerFactory.getLogger(HelloLogger.class);
+	public static void main(String[] args) {
+		System.out.println("Hello, Logger at" + new Date());
+		logger.info("Hello, Logger. <Logger info>");
+
+	}
+
+}
